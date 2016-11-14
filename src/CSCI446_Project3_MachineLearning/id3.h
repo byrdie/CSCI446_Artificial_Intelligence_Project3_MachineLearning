13fa8ed0679bc9a5bf17280ce0ae769bac57a80c
Nevin <nevinleh@gmail.com>
2016-11-14 13:33:46 -0700

    Added file for id3

---
+++ src/CSCI446_Project3_MachineLearning/id3.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   id3.h
+ * Author: girish
+ *
+ * Created on November 14, 2016, 1:27 PM
+ */
+
+#ifndef ID3_H
+#define ID3_H
+
+
+
+#endif /* ID3_H */
+
