529ead8d99e60ea0439d061302e4a459fd22e27d
marshmmallow <bmarsh7@bresnan.net>
2016-11-17 11:48:51 -0700

    Create nearest_neighbor.h

---
+++ src/CSCI446_Project3_MachineLearning/nearest_neighbor.h
@@ -0,0 +1,28 @@
+
+/* 
+ * File:   nearest_neighbor.h
+ * Author: brianmarsh
+ *
+ * Created on November 16, 2016, 6:56 PM
+ */
+
+#ifndef NEAREST_NEIGHBOR_H
+#define NEAREST_NEIGHBOR_H
+
+#include "learn.h"
+#include <math.h>
+
+class NearestNeighbor : public Learner {
+public:
+    NearestNeighbor(Dataset train_data);
+    void learn();
+    uint answer(datum attrs);
+private:
+    vector<vector<uint>> storedValues;
+    vector<vector<uint>> distances; 
+    int numNeighbors;
+    
+};
+
+#endif /* NEAREST_NEIGHBOR_H */
+
